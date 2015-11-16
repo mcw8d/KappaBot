90495658ef6096b2d0ab52b9c8862e2bc13ca526
Edward Brown <edb5e@dhcp35.ccri.com>
2015-11-16 18:33:04 -0500

    Made the countdown method not just display millis

--- src/main/ts/commands/countdown.ts
+++ src/main/ts/commands/countdown.ts
@@ -4,7 +4,17 @@ import Q = require('q');
 class Countdown implements IKappaCommand {
     execute (args: string[]): Q.Promise<string> {
 	var countdown = new Date("2015-11-20T18:00:00.000Z").getTime() - new Date().getTime();
-        return Q.when(countdown.toString());
+
+        var x = countdown / 1000;
+        var seconds = Math.floor(x % 60);
+	x /= 60;
+        var minutes = Math.floor(x % 60);
+        x /= 60;
+        var hours = Math.floor(x % 24);
+        x /= 24;
+        var days = Math.floor(x);
+	var output = ("Days until Novlovplov: " + days + " Days," + hours + " Hours," + minutes + " Minutes, and " + seconds + " Seconds.");
+        return Q.when(output);
     }
 
     describe (): string {
